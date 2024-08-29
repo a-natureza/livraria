453831e0151e7527ae64e24881cfe58f53fb70bf
a-natureza <97416824+a-natureza@users.noreply.github.com>
2024-08-29 19:58:57 -0300

    # Changes to be committed: #	new file:   .gitignore #	new file:   README.md #	new file:   backend/db.json #	new file:   eslint.config.js #	new file:   index.html #	new file:   package-lock.json #	new file:   package.json #	new file:   public/vite.svg #	new file:   src/App.jsx #	new file:   src/assets/react.svg #	new file:   src/forms/formulario.jsx #	new file:   src/main.jsx #	new file:   src/modelo/livro.js #	new file:   vite.config.js

---
+++ src/forms/formulario.jsx
@@ -0,0 +1,10 @@
+import { useForm } from "react-hook-form"
+
+const {register, watch, handleSubimit, formState: { errors }} = useForm()
+
+
+const Formulário = () => {
+
+}
+
+export default Formulário
